086c5fc817f4acf3d1403cd4b4d4c15d69d06114
toopazo <toopazo@protonmail.com>
2021-12-02 20:59:58 -0500

    testing mavlink

--- mavlink_custom.py
+++ mavlink_custom.py
@@ -133,7 +133,7 @@ class FireflyMavCmd:
     def check_timeout():
         time_now = datetime.datetime.now()
         delta = time_now - FireflyMavCmd.time_prev
-        if delta >= 60:
+        if delta.seconds >= 60:
             return True
         else:
             return False
